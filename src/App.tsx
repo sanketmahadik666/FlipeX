265e2dde66aa0b072afab427eb8be5207c5494b9
sanketmahadik666 <mahadiks666@gmail.com>
2026-02-14 23:15:02 +0530

    Add main application structure with routing, styling, and design system

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider as ReduxProvider } from "react-redux";
+import { RecoilRoot } from "recoil";
+import { store } from "@/store/store";
+import Landing from "./pages/Landing";
+import Upload from "./pages/Upload";
+import ExperienceSelector from "./pages/ExperienceSelector";
+import Reader from "./pages/Reader";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <ReduxProvider store={store}>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Landing />} />
+              <Route path="/upload" element={<Upload />} />
+              <Route path="/experience" element={<ExperienceSelector />} />
+              <Route path="/reader" element={<Reader />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </TooltipProvider>
+      </QueryClientProvider>
+    </RecoilRoot>
+  </ReduxProvider>
+);
+
+export default App;
